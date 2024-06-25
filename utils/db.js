76dc65b831617b3d9b17b56c10f71ab104d9ff64
Majithia Devansh <123489552+mdevansh3012@users.noreply.github.com>
2024-06-25 20:29:56 +0530

    changes in db.js

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,4 @@
-/* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/no-var-requires */
-// server/db.js
+
 require("dotenv").config();
 const mysql = require("mysql2");
 
@@ -9,10 +7,11 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-// eslint-disable-next-line no-undef
+// Export the pool with promises enabled
 module.exports = pool.promise();
